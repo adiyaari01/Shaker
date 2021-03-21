150be451d659eebfd086a997c5897ba45177aa4a
Adi Yaari <adiyaari01@gmail.com>
2021-03-21 09:00:32 +0200

    delete comments

--- projects/models.py
+++ projects/models.py
@@ -26,21 +26,4 @@ def __str__(self):
     return self.projectName
 
 
-#from django.db import models
-#from django.contrib.auth.models import User
-## Create your models here.
-#
-#class Project(models.Model):
-#    user_id = models.ForeignKey(User, on_delete=models.CASCADE) #relationship 1:n (user:todos)
-#    dateCreated = models.DateField(auto_now_add=True) #automatic
-#    fullName = models.CharField(max_length=70)
-#    email = models.CharField(max_length=70)
-#    phone = models.CharField(max_length=10)
-#    projectName = models.CharField(max_length=70)
-#    keyWord = models.CharField(max_length=70)
-#    description = models.TextField(blank=True)
-#    image = models.FileField(upload_to='uploads/')
-#
-#
-#def __str__(self):
-#    return self.projectName
+
